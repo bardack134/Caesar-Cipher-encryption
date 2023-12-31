aeb229a1300ae7e8ec3dda58b17bb6a60e33c5d0
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-01-01 08:07:51 +0900

    final logica

---
+++ decrypt.py
@@ -0,0 +1,45 @@
+# Definir una función llamada 'decrypt' que toma el 'text' y el 'shift' como parámetros.
+def decrypt(text, shift):
+    
+    # Crear una lista con las letras del alfabeto
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+
+    # Pedir al usuario que introduzca el mensaje que quiere desencriptar y asignar su respuesta a una variable llamada text. Convertir el mensaje a minúscula.
+    # En este caso, el mensaje es "hello world" y está asignado directamente a la variable text
+    # text = input("Type your message:\n").lower()
+    
+
+    # Crear una lista vacía para guardar el nuevo alfabeto desplazado
+    new_alphabet_list=[]
+    
+    # Recorrer cada letra del alfabeto original desde la posición indicada por el 'shift' hasta el final
+    for i in range(shift, len(alphabet)):
+        # Añadir cada letra a la lista del nuevo alfabeto
+        new_alphabet_list.append(alphabet[i])
+        
+        # Comprobar si la letra es la última del alfabeto original
+        if i == alphabet.index("z"):
+            # Si es así, recorrer cada letra del alfabeto original desde el principio hasta la posición indicada por el 'shift'
+            for j in range(0, shift):
+                # Añadir cada letra a la lista del nuevo alfabeto
+                new_alphabet_list.append(alphabet[j])
+    
+    
+    
+    # Crear una lista vacía para guardar el mensaje desencriptado
+    secret_code_list=[]
+    
+    # Recorrer cada letra del mensaje encriptado
+    for i in text:
+        
+        # Obtener la posición de la letra de acuerdo a la lista del nuevo alfabeto y asignarla a una variable llamada position_letter
+        position_letter=new_alphabet_list.index(i)
+        
+        # Añadir la letra de acuerdo al alfabeto original a la lista del mensaje desencriptado
+        secret_code_list.append(alphabet[position_letter])
+        
+    # Convertir la lista del mensaje desencriptado en una cadena y asignarla a una variable llamada secret_code_string
+    secret_code_string="".join(secret_code_list)
+    
+    # Imprimir el mensaje desencriptado
+    print(f"secret code is {secret_code_string}")
--- main.py
+++ main.py
@@ -1,1 +1,83 @@
-"probando"
+
+# Importar los módulos encrypt y decrypt que contienen las funciones para encriptar y desencriptar el mensaje
+from encrypt import *
+
+from decrypt import *
+
+from art import *
+
+#impormimos logo del codigo
+print(logo)
+
+# Pedir al usuario que elija si quiere encriptar o desencriptar un mensaje y asignar su respuesta a una variable llamada direction
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+
+# Pedir al usuario que introduzca el mensaje que quiere encriptar o desencriptar y asignar su respuesta a una variable llamada text. Convertir el mensaje a minúscula.
+text = input("Type your message:\n").lower()
+
+# Crear una variable booleana llamada verification_text que indica si el mensaje es válido o no. Inicializarla en False
+verification_text=False
+
+# Crear un bucle while que se repita mientras el mensaje no sea válido
+while verification_text==False:
+    
+    # Recorrer cada carácter del mensaje
+    for i in text:
+        
+       # Comprobar si el carácter es un dígito
+       if i.isdigit():
+          # Si es así, pedir al usuario que introduzca el mensaje de nuevo y asignarlo a la variable text
+          text = input("you enter a incorrect data, please enter your message again: ")
+           
+       # Si no es un dígito, cambiar el valor de la variable verification_text a True
+       else:
+           verification_text=True
+    
+
+    
+
+# Pedir al usuario que introduzca el número de posiciones que quiere desplazar las letras y asignar su respuesta a una variable llamada shift. Convertir la respuesta a un número entero.
+shift = int(input("Type the shift number:\n"))
+
+# Imprimir una línea vacía para separar
+print('')
+
+# Comprobar si el usuario eligió encriptar el mensaje
+if direction == 'encode':
+    # Si es así, llamar a la función encrypt del módulo encrypt con el mensaje y el desplazamiento como argumentos
+    encrypt(text, shift)
+
+# Comprobar si el usuario eligió desencriptar el mensaje
+elif direction == 'decode':
+    # Si es así, llamar a la función decrypt del módulo decrypt con el mensaje y el desplazamiento como argumentos
+    decrypt(text, shift)
+    
+# Si el usuario no eligió ninguna de las dos opciones
+else:
+    # Imprimir un mensaje de error que le dice al usuario que no introdujo 'encode' o 'decode'
+    print("you didn't enter 'encode' or 'decode', please try again")
+    # Pedir al usuario que elija de nuevo y asignar su respuesta a la variable direction
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    # Imprimir una línea vacía para separar
+    print()
+    # Crear un bucle while que se repita mientras el usuario no elija una opción válida
+    while direction != 'encode' and direction != 'decode':
+        # Imprimir un mensaje de error que le dice al usuario que no introdujo 'encode' o 'decode'
+        print("you didn't enter 'encode' or 'decode', please try again")
+        # Pedir al usuario que elija de nuevo y asignar su respuesta a la variable direction
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        # Imprimir una línea vacía para separar
+        print()
+
+        # Comprobar si el usuario eligió encriptar el mensaje
+        if direction == 'encode':
+            # Si es así, llamar a la función encrypt del módulo encrypt con el mensaje y el desplazamiento como argumentos
+            encrypt(text, shift)
+
+        # Comprobar si el usuario eligió desencriptar el mensaje
+        elif direction == 'decode':
+            # Si es así, llamar a la función decrypt del módulo decrypt con el mensaje y el desplazamiento como argumentos
+            decrypt(text, shift)       
+    
+
